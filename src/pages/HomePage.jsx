39085e6423bda5989993f8a0effa8a64b44e1742
Erfanmah7 <efn8mah@yahoo.com>
2024-05-28 23:34:13 +0330

    add NavLink

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/courses">Courses</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about-us">About Us</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default HomePage;
