0fabe490ee99c142a645447a458a272bff99c162
Erfanmah7 <efn8mah@yahoo.com>
2024-05-28 23:16:47 +0330

    add BrowserRouter,Routes,Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Courses from "./pages/Courses";
+import AboutUs from "./pages/AboutUs";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/about-us" element={<AboutUs />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
